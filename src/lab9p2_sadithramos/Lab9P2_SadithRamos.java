ef106fb688febd4655c9978fd9084de4f16167bc
Alejo2r <sadith.alejandro@gmail.com>
2024-12-06 13:16:19 -0600

    Inicio del LAB

---
+++ src/lab9p2_sadithramos/Lab9P2_SadithRamos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab9p2_sadithramos;
+
+/**
+ *
+ * @author Admin
+ */
+public class Lab9P2_SadithRamos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
